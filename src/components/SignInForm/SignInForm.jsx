74bc5cf16653f42f09043ad41616e2ca1e756f22
Fernando-CR19 <fernandocr019@gmail.com>
2022-11-17 18:43:57 -0300

    validação do sign in

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useFormik } from 'formik';
+import Field from '../Field';
 
-export default function SignInForm() {
+export default function SignInForm({ handleSubmit, handleChange, handleBlur, isSubmitting, values, errors}) {
 
     const formik = useFormik({
         initialValues: {
@@ -16,24 +17,36 @@ export default function SignInForm() {
       });
 
   return (
-    <div className='ml-auto mr-auto mt-36 w-3/5 rounded-md'>
-        <form onSubmit={formik.handleSubmit}>
-            <div className=''>
-                <h4 className='text-4xl mb-4 ml-auto text-center'>Sign In</h4>
-                <p className='mb-4 text-center'>Not registered on Marvel yet? <Link to='/signup' className='underline underline-offset-1 text-blue-500'>SIGN UP</Link></p>
-            </div>
-            <div className='flex flex-col gap-4'>
-                <div className='flex flex-col'>
-                    <label className='mb-1'>Email</label>
-                    <input id="Email" type="email" name="Email" onChange={formik.handleChange} value={formik.values.Email} className='w-full border-black border-2 rounded p-1'/>
-                </div>
-                <div className='flex flex-col'>
-                    <label className='mb-1'>Password</label>
-                    <input id="Password" type="password" name="Password" onChange={formik.handleChange} value={formik.values.Password} className='w-full border-black border-2 mb-4 rounded p-1'/>
-                </div>
-            </div>
-            <button type ="submit" className='w-full bg-blue-800 text-white p-2 rounded'>SIGN IN</button>
-        </form>
-    </div>
+    <form onSubmit={handleSubmit} className="max-w-lg mx-auto h-full flex flex-col justify-center">
+      <h3 className="text-lg text-center">Sign in</h3>
+      <div className="space-y-4">
+        <Field
+          label="Email"
+          type="text"
+          name="email"
+          value={values.email}
+          placeholder="your@email"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          errorMessage={errors.email}
+        />
+        <Field
+          label="Password"
+          type="password"
+          name="password"
+          value={values.password}
+          placeholder="your password"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          errorMessage={errors.password}
+        />
+        <button
+          type="submit"
+          className="text-red-800 bg-gray-100 px-4 py-4 rounded-lg w-full"
+        >
+          Sign in
+        </button>
+      </div>
+    </form>
   )
 }
--- src/containers/views/SignInViewContainer.jsx
+++ src/containers/views/SignInViewContainer.jsx
@@ -1,10 +1,18 @@
 import SignInForm from "../../components/SignInForm/SignInForm"
 import { View } from "../../components/View"
+import { useSignInForm } from "../../components/SignInForm/useSignInForm"
 
 export const SignInViewContainer = () => {
+
+    const form = useSignInForm({
+        onSubmit: (values) => {
+            console.log("Sign in form values", values)
+        }
+    })
+
     return <div>
-    <View>
-        <SignInForm/>
-    </View>
+        <View>
+            <SignInForm {...form}/>
+        </View>
     </div>
 }

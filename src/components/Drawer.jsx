cd5d87848f0cc133cda56bdd7c0d2e284be5b15d
Fernando-CR19 <fernandocr019@gmail.com>
2023-02-09 18:56:01 -0300

    Accordion

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,21 +1,48 @@
-import clsx from "clsx"
+import clsx from "clsx";
 
-export const Drawer = ({ position = "right", show = false, onClick }) => {
+export const Drawer = ({ show = false, position = "right", onClick }) => {
+    const drawerClassNames = () => {
+        if (position === "left") {
+            return clsx(
+                "fixed w-72 h-screen transition bg-white left-0",
+                show ? "translate-x-0" : "-translate-x-full"
+            );
+        }
+
+        if (position === "right") {
+            return clsx(
+                "fixed w-72 h-screen transition bg-white right-0",
+                show ? "translate-x-0" : "translate-x-full"
+            );
+        }
+
+        if (position === "top") {
+            return clsx(
+                "fixed top-0 transition w-screen h-72 bg-white",
+                show ? "translate-y-0" : "-translate-y-full"
+            );
+        }
+
+        if (position === "bottom") {
+            return clsx(
+                "fixed bottom-0 transition w-screen h-72 bg-white",
+                show ? "translate-y-0" : "translate-y-full"
+            );
+        }
+
+        return ""
+    };
+
     return (
         <div>
-            <div className={clsx("fixed left-0 top-0 w-screen h-screen bg-gray-800/90",
-                show ? "block" : "hidden",
-            )}
-                onClick={onClick}></div>
-            <div className={clsx(
-                "fixed bg-white transition duration-300 ease-in-out",
-                position === "left" || position === "right" ? "w-72 h-screen" : "",
-                show && position === "left" ? "translate-x-0" : "-translate-x-full",
-                show && position === "right" ? " right-0 -translate-x-0" : "right-0 translate-x-full",
-                position === "top" || position === "bottom" ? "h-72 w-screen" : "",
-                position === "top" && "top-0",
-                position === "bottom" && "bottom-0"
-            )}>Drawer</div>
-        </div >
-    )
-} 
+            <div
+                onClick={onClick}
+                className={clsx(
+                    "fixed  w-screen h-screen transition-all duration-300 bg-gray-900/95",
+                    show ? "visible opacity-100" : "invisible opacity-0"
+                )}
+            ></div>
+            <div className={drawerClassNames()}>Drawer</div>
+        </div>
+    );
+};
--- src/containers/views/HomeViewContainer.jsx
+++ src/containers/views/HomeViewContainer.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { View } from "../../components/View";
 import { CharactersGrid } from "../../components/CharactersGrid";
 import { Drawer } from "../../components/Drawer";
+import { Accordion } from "../../components/Accordion";
 
 import { useFetchApi } from "../../hooks/useFetchApi";
 
@@ -9,6 +10,44 @@ export const HomeViewContainer = () => {
   const [characters, setCharacters] = useState([]);
   const { data, startFetch } = useFetchApi("/api/characters");
   const [show, setShow] = useState(false)
+  const [items, setItems] = useState([
+    {
+      id: 0,
+      title: "item 1",
+      open: false,
+      content: <div>Conteudo 1</div>
+    },
+    {
+      id: 1,
+      title: "item 2",
+      open: false,
+      content: <div>Conteudo 2</div>
+    },
+    {
+      id: 2,
+      title: "item 3",
+      open: false,
+      content: <div>Conteudo 3</div>
+    },
+  ])
+
+  const onAccordionToggle = (id) => {
+    setItems((prev) => {
+      return prev.map((item) => {
+        return {
+          ...item,
+          open: item.id === id ? !item.open : false
+        }
+        // if (item.id === id) {
+        //   return {
+        //     ...item,
+        //     open: !item.open
+        //   }
+        // }
+        // return item;
+      })
+    })
+  }
 
   const onClick = () => {
     setShow(!show)
@@ -31,6 +70,7 @@ export const HomeViewContainer = () => {
     <>
       <Drawer show={show} onClick={onClick} />
       <View>
+        <Accordion items={items} onToggle={onAccordionToggle} />
         <button onClick={onClick}>toggle Drawer</button>
         <CharactersGrid characters={characters} />
       </View>
